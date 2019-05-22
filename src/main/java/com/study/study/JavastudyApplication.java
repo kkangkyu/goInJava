f266cd7a0b8cd754693cc816d9e1f86d57991a4c
강규선(Kyusun Kang)/Finding개발팀/11ST <way1024@sk.com>
2019-05-22 18:04:29 +0900

    Initial Commit

---
+++ src/main/java/com/study/study/JavastudyApplication.java
@@ -0,0 +1,13 @@
+package com.study.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavastudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavastudyApplication.class, args);
+    }
+
+}
